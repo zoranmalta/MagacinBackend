45f622abe9a0e33d7cd650ef02cb2db041f9accf
Zoran Marcetic <zoranmarcetic@hotmail.com>
2020-05-14 00:25:43 +0200

    inicijalni komit

---
+++ src/main/java/sf/MagacinBackend/MagacinBackendApplication.java
@@ -0,0 +1,18 @@
+package sf.MagacinBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagacinBackendApplication {
+
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(MagacinBackendApplication.class, args);
+		System.out.println("hello");
+
+	}
+
+}
---
+++ src/test/java/sf/MagacinBackend/MagacinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sf.MagacinBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagacinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
